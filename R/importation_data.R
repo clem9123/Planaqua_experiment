fb66388b18fcf20652a7519239935f7db1652a9c
clem9123 <chouchoutte908@gmail.com>
2022-03-28 19:14:54 +0200

    ordi mark

--- R/importation_data.R
+++ R/importation_data.R
@@ -13,9 +13,8 @@ library(sjmisc)
 library(sjPlot)
 library(stats)
 
-# importation des données et mise en forme des colonnes
-setwd("C:/Users/sandrine/Documents/IEES_stage/code_et_donnee")
-BDD_f <- read_excel("data/data_final_ws_norm.xlsx", col_types = c("numeric","date","text","text","text","text","text","text","text","numeric","numeric","text","text"), na = "")
+# importation des donn?es et mise en forme des colonnes
+BDD_f <- read_excel("./data/data_final_ws_norm.xlsx", col_types = c("numeric","date","text","text","text","text","text","text","text","numeric","numeric","text","text"), na = "")
 BDD_f <- BDD_f %>% mutate(Lake_capture = as.factor(Lake_capture),
                           Method_capture =as.factor(Methode_capture),
                           Session_capture = as.factor(Session_capture),
@@ -29,7 +28,7 @@ summary(BDD_f)
 BDD_f <- BDD_f %>% mutate (Lake_capture = factor(Lake_capture,levels = c(1,8,3,6,11,14,9,16,2,7,4,5,12,13,10,15)))
 BDD_f <- BDD_f %>% mutate (Lake_released = factor(Lake_released,levels = c(1,8,3,6,11,14,9,16,2,7,4,5,12,13,10,15)))
 
-# ajout de la colonne année 
+# ajout de la colonne ann?e 
 BDD_f <- BDD_f %>% mutate (Year = as.factor(format(Date,"%Y")))
 
 # ajout des colonnes de traitement des lacs : Nutrients, Perch, Treatment 
@@ -42,12 +41,12 @@ BDD_f <- BDD_f %>% mutate (Lake = case_when(is.na(Lake_capture) ~ Lake_released,
 BDD_f <- merge (BDD_f, Lake_treatment)
 BDD_f <- BDD_f %>% arrange(Index)
 
-# création d'une colonne log_weight et Log_size
+# cr?ation d'une colonne log_weight et Log_size
 BDD_f <- BDD_f %>% mutate(Log_weight = log(Weight))
 BDD_f <- BDD_f %>% mutate(Log_size = log(Size))
 
-# création du tableau gardon adulte pris dans les chaluts lors des sessions classiques
-# ajout de la colonne année tag
+# cr?ation du tableau gardon adulte pris dans les chaluts lors des sessions classiques
+# ajout de la colonne ann?e tag
 BDD_a <- BDD_f %>% filter(Species == "gardon" & 
                         Tag_id != "juvenile" &
                         !(Method_capture %in% c("Nasse")) &
@@ -64,7 +63,7 @@ BDD_a <- BDD_a %>% mutate (Obs_status = case_when(Tag_year %in% c(NA,"juvenile")
                                                   Year == Tag_year ~ "capture",
                                                   Year != Tag_year ~ "recapture"))
 
-# création du même tableau que ci dessus mais avec les juvéniles en plus
+# cr?ation du m?me tableau que ci dessus mais avec les juv?niles en plus
 BDD_j <- BDD_f %>% filter(Species == "gardon" &
                           !(Method_capture %in% c("Nasse")) &
                           !(Session_capture %in% c("B","Z"))&
@@ -79,7 +78,7 @@ BDD_j <- BDD_j %>% mutate (Obs_status = case_when(Tag_year %in% c(NA,"juvenile")
                                                   Year == Tag_year ~ "capture",
                                                   Year != Tag_year ~ "recapture"))
 
-# création d'un tableau avec les nombres de poisson par année et lac avec les poids et taille moyenne
+# cr?ation d'un tableau avec les nombres de poisson par ann?e et lac avec les poids et taille moyenne
 
 BDD_n <- BDD_a %>% group_by(Year, Lake, Nutrients, Perch, Treatment) %>%
                      summarise(Mean_logweight = mean(Log_weight, na.rm =T),
