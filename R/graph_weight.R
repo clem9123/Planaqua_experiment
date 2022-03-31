4016483cbe9690123585cf10ea48893f38f9f12d
clem9123 <chouchoutte908@gmail.com>
2022-03-31 17:07:13 +0200

    Create graph_weight.R

---
+++ R/graph_weight.R
@@ -0,0 +1,19 @@
+ggplot(BDD_a %>% filter(Year == "2019" & Tag_year == "2019"), aes ( x = Weight))+
+  geom_bar(aes(x = Weight, color = Weight == 58))
+
+ggplot(BDD_a %>% filter(Year == "2019"), aes ( x = Weight))+
+  geom_bar(aes(x = Weight))+
+  xlim(60,200)
+
+ggplot(BDD_a %>% filter( Tag_year == "2016"))+
+  geom_density(aes(x  = Log_size, color = Year))
+
+ggplot(BDD_a %>% filter( Tag_year == "2016"))+
+  geom_boxplot(aes(x = Year, y = Log_weight, color = Treatment))
+
+ggplot(BDD_a %>% filter(Tag_year == "2019"))+
+  geom_boxplot(aes(x = Year, y = Log_weight, color = Treatment))
+
+ggplot(BDD_a %>% filter( Tag_year == "2016"), aes(x = Year, y  = Weight, color = Tag_id))+
+  geom_point()+
+  geom_line()
