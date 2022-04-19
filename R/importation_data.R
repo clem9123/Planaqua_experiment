6022e8c0fc7ba47a8532912201ec45c8a23331a0
clem9123 <chouchoutte908@gmail.com>
2022-04-19 11:47:26 +0200

    work on cleaning data

--- R/importation_data.R
+++ R/importation_data.R
@@ -6,7 +6,7 @@ library(readxl)
 # importation des donn?es et mise en forme des colonnes
 BDD_f <- read_excel("./data/data_final_ws_norm.xlsx", col_types = c("numeric","date","text","text","text","text","text","text","text","numeric","numeric","text","text"), na = "")
 BDD_f <- BDD_f %>% mutate(Lake_capture = as.factor(Lake_capture),
-                          Method_capture =as.factor(Methode_capture),
+                          Method_capture =as.factor(Method_capture),
                           Session_capture = as.factor(Session_capture),
                           Passage_net = as.factor(Passage_net),
                           Obs_status = as.factor(Obs_status),
@@ -35,42 +35,29 @@ BDD_f <- BDD_f %>% arrange(Index)
 BDD_f <- BDD_f %>% mutate(Log_weight = log(Weight))
 BDD_f <- BDD_f %>% mutate(Log_size = log(Size))
 
-# cr?ation du tableau gardon adulte pris dans les chaluts lors des sessions classiques
-# ajout de la colonne ann?e tag
-BDD_a <- BDD_f %>% filter(Species == "gardon" & 
-                        Tag_id != "juvenile" &
-                        !(Method_capture %in% c("Nasse")) &
-                        !(Session_capture %in% c("B","Z")) &
-                          (Year == "2016" | Obs_status != "introduction")) %>% 
-                  group_by(Tag_id) %>% 
-                  mutate(Tag_year = case_when(Tag_id == "juvenile" ~ "juvenile" ,
-                                              is.na(Tag_id) ~ "NA",
-                                              TRUE ~ min(as.character(Year))))
-BDD_a$Tag_year <- BDD_a$Tag_year %>% na_if("NA")
+# Ajout de la colonne Tag_year
+BDD_f <- BDD_f %>% mutate(Tag_year = ifelse(Tag_id ==  "no_tag", NA, min(as.character(Year))))
 
-## modification de la colonne Obs_status en ne prenant pas en compte les captures par Nasse
-BDD_a <- BDD_a %>% mutate (Obs_status = case_when(Tag_year %in% c(NA,"juvenile")~ "capture",
+# creation du tableau roach adulte pris dans les trawls lors des sessions classiques
+# ajout de la colonne annee tag
+
+
+BDD_g <- BDD_f %>% filter(Species == "roach" & !(Method_capture %in% c("creel")) &
+                            !(Session_capture %in% c("B","Z")))
+BDD_g <- BDD_g %>% mutate (Obs_status = case_when(Tag_year %in% c("no_tag","juvenile")~ "capture",
                                                   Year == Tag_year ~ "capture",
                                                   Year != Tag_year ~ "recapture"))
 
-# cr?ation du m?me tableau que ci dessus mais avec les juv?niles en plus
-BDD_j <- BDD_f %>% filter(Species == "gardon" &
-                          !(Method_capture %in% c("Nasse")) &
-                          !(Session_capture %in% c("B","Z"))&
-                          (Year == "2016" | Obs_status != "introduction")) %>% 
-                    group_by(Tag_id) %>% 
-                    mutate(Tag_year = case_when(Tag_id == "juvenile" ~ "juvenile" ,
-                                      is.na(Tag_id) ~ "NA",
-                                      TRUE ~ min(as.character(Year))))
-BDD_j$Tag_year <- BDD_j$Tag_year %>% na_if("NA")
 
-BDD_j <- BDD_j %>% mutate (Obs_status = case_when(Tag_year %in% c(NA,"juvenile") ~ "capture",
+BDD_p <- BDD_f %>% filter(Species == "perch" & !(Method_capture %in% c("creel")) &
+                            !(Session_capture %in% c("B","Z")))
+BDD_p <- BDD_p %>% mutate (Obs_status = case_when(Tag_year %in% c("no_tag","juvenile")~ "capture",
                                                   Year == Tag_year ~ "capture",
                                                   Year != Tag_year ~ "recapture"))
 
 # cr?ation d'un tableau avec les nombres de poisson par ann?e et lac avec les poids et taille moyenne
 
-BDD_n <- BDD_a %>% group_by(Year, Lake, Nutrients, Perch, Treatment) %>%
+BDD_n <- BDD_g %>% group_by(Year, Lake, Nutrients, Perch, Treatment) %>%
                      summarise(Mean_logweight = mean(Log_weight, na.rm =T),
                                Mean_logsize = mean (Log_size, na.rm =T),
                                Mean_weight = mean(Weight, na.rm =T),
@@ -79,7 +66,3 @@ BDD_n <- BDD_a %>% group_by(Year, Lake, Nutrients, Perch, Treatment) %>%
                                nb_juv = sum(Tag_id == "juvenile", na.rm = T),
                                biomasse = sum(Weight, na.rm =T)) %>%
                      ungroup()
-
-
-
-
