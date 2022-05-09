7b95e8ed30bbe1173915dff22d295e46968c811f
clem9123 <chouchoutte908@gmail.com>
2022-05-09 11:50:42 +0200

    work

--- R/setup.R
+++ R/setup.R
@@ -1,8 +1,10 @@
 library(R2jags)
 library(tidyverse)
 library(arules)
-
+library(patchwork)
 source("R/importation_data.R")
+load("~/R/object/Model_multi_treatment_corrected_abundance.RData")
+
 s <- BDD_a %>% ungroup () %>% pivot_wider(id_cols = Tag_id, names_from = Year, values_from = Size)
 
 source("R/creation_tableau_taille_complet.R")
