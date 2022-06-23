8934741358506db579e20aa9315ed372b11164ec
clem9123 <chouchoutte908@gmail.com>
2022-06-23 16:26:51 +0200

    big cleaning

---
+++ R/setup.R
@@ -0,0 +1,18 @@
+# -----------------
+# Necessary library
+# -----------------
+
+library(R2jags)
+library(tidyverse)
+library(arules)
+library(patchwork)
+library(ggalluvial)
+library(LaplacesDemon)
+library(lme4)
+library(scales)
+
+# ----------------
+# Data importation
+# ----------------
+
+source("R/importation_data.R")
